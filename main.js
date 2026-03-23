742b7424cc65d9bc95e9b567b1f0d082863bf309
Brian-Rotich128 <markguruu@gmail.com>
2026-03-23 14:10:14 +0300

    all changes in js, home page and style

---
+++ main.js
@@ -0,0 +1,11 @@
+function details() {
+alert("Employee details will appear here.");
+}
+
+function edit() {
+alert("Edit employee information.");
+}
+
+function deleteContact() {
+confirm("Are you sure you want to delete?");
+}
